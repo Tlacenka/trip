28e97c349be4f3edf4c827049ed2a25f94b8c832
Jan Trnavský <jan.trnavsky@roihunter.com>
2025-08-14 08:03:13 +0200

    update production path

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    reporters: ['default'],
-    globals: true,
-    environment: 'node',
-    include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    watch: false,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/trip/' : '/',
+    plugins: [react()],
+    test: {
+      reporters: ['default'],
+      globals: true,
+      environment: 'node',
+      include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      watch: false,
+    },
+  };
 });
